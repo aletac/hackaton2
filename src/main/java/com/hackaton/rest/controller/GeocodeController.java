9662d68288a0f7beb3905c546144a763aa7b211e
Alejandro Tacue <adolfo.tacue.mt@correo.usa.edu.co>
2021-12-04 09:39:08 -0500

    Initial commit

---
+++ src/main/java/com/hackaton/rest/controller/GeocodeController.java
@@ -0,0 +1,34 @@
+package com.hackaton.rest.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hackaton.rest.model.GeocodeResult;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class GeocodeController {
+    @RequestMapping(path = "/geocode", method = RequestMethod.GET )
+    public GeocodeResult getGeocode(@RequestParam String address) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://google-maps-geocoding.p.rapidapi.com/geocode/json?latlng=4.630472%2C-74.141389&language=en")
+                .get()
+                .addHeader("x-rapidapi-host", "google-maps-geocoding.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "e183818913mshe97aa8862df0942p1550eajsn6bfb6caff241")
+                .build();
+
+        ResponseBody responseBody = client.newCall(request).execute().body();
+        ObjectMapper objectMapper = new ObjectMapper();
+        GeocodeResult result = objectMapper.readValue(responseBody.string(), GeocodeResult.class);
+        return result;
+    }
+}
+
---
+++ src/main/java/com/hackaton/rest/model/GeocodeResult.java
@@ -0,0 +1,19 @@
+package com.hackaton.rest.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeocodeResult {
+
+    List<GeocodeObject> results;
+    String status;
+
+}
